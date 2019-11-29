51261c4ac7dc930e511b592aa9c42507ff5d43dc
pacher <paul.pacher@oenb.at>
2019-11-29 15:14:22 +0100

    ohne output weiterleiten funktionierts ganz gut

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 
 var Esc string = "\x1b"
 var RUNE_A rune = '\x3d'
+var RUNE_LF rune = 0xA
+var CMD_SEND []rune = []rune{0x73, 0x65, 0x6E, 0x64, 0xD, 0xA}
+var CMD_EXIT []rune = []rune{0x65, 0x78, 0x69, 0x74, 0xD, 0xA}
 
 func main() {
 
@@ -17,47 +20,75 @@ func main() {
 
 	pr, pw := io.Pipe()
 	c1 := make(chan int)
-	//c2 := make(chan int)
 	go writeToPipe(pw, c1)
 	go readFromPipe(pr, c1)
 
 	<-c1
-	//<-c2
 
-	fmt.Println("Bye")
+	fmt.Println("Tschüssikovski!")
 }
 
 func writeToPipe(pw *io.PipeWriter, ch chan int) {
 	var data []rune
+	var command []rune
 	var err error
 	var r rune
-
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		if r, _, err = reader.ReadRune(); err != nil {
-			panic(err)
+		if r, _, err = reader.ReadRune(); errors.Is(io.EOF, err) {
+			fmt.Println("EOF!")
+			command = CMD_SEND
+			r = RUNE_LF
+		} else {
+			command = append(command, r)
 		}
-		data = append(data, r)
-		if r == 's' {
-			if _, err = pw.Write([]byte(string(data))); err != nil {
-				panic(err)
+
+		if r == RUNE_LF {
+			if compareRune(command, CMD_SEND) {
+				doWrite(pw, ch, data)
+				data = make([]rune, 0) // Daten zum senden
+			} else if compareRune(command, CMD_EXIT) {
+				break
+			} else {
+				data = append(data, command...)
 			}
-		} else if r == 'q' {
-			pw.Close()
+			command = make([]rune, 0) // Neuer Befehl
+		}
+		if errors.Is(io.EOF, err) {
 			break
 		}
 	}
+	fmt.Println("Leitung wird an der Senderseite geschlossen!")
+	pw.Close()
 	ch <- 1
+}
 
+func compareRune(a, b []rune) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func doWrite(pw *io.PipeWriter, ch chan int, data []rune) {
+	if _, err := pw.Write([]byte(string(data))); errors.Is(io.ErrClosedPipe, err) {
+		fmt.Println("Schreiben in die Leitung nicht möglich. Pipe wurde geschlossen!")
+		ch <- 1
+	}
 }
 
 func readFromPipe(pr *io.PipeReader, ch chan int) {
-	//var in []byte
-	//bw := bufio.NewWriter(os.Stdout)
-	if _, err := io.Copy(os.Stdout, pr); errors.Is(err, io.EOF) {
-		//fmt.Println(in)
-		pr.Close()
-		//bw.Flush()
+
+	defer pr.Close()
+	if _, err := io.Copy(os.Stdout, pr); errors.Is(err, io.ErrClosedPipe) {
+		fmt.Println("Unerwartetes Ende der Leitung. Wurde geschlossen!")
 		ch <- 1
 	}
+	fmt.Println("Pipe wurde geschlossen!")
+	ch <- 1
 }

7ebe23eb4b63df5f0bfec1c69a0fd5c11ca92eff
pacher <paul.pacher@oenb.at>
2019-12-05 15:29:42 +0100

    alternative #1

--- main.go
+++ main.go
@@ -1,79 +1,86 @@
 package main
 
 import (
+	"bufio"
 	//"bufio"
 	"errors"
 	"fmt"
+	"github.com/mattn/go-tty"
 	"io"
 	"os"
-	"github.com/mattn/go-tty"
+	"github.com/mattn/go-colorable"
 )
 
-var esc string = "\x1b"
+const ESC rune = 0x1b
+
+var csi rune = 0x9b // CSI
+const LEFT_SQUARE_BRACKET rune = 0x5B
+const SEMI_COLUMN rune = 0x3B
+const M_HEX rune = 0x6D
+
 var runeA rune = '\x3d'
 var runeLf rune = 0xA
 var runeCarriageReturn rune = 0xD
 var cmdSend []rune = []rune{0x73, 0x65, 0x6E, 0x64, 0xD}
 var cmdExit []rune = []rune{0x65, 0x78, 0x69, 0x74, 0xD}
 
+//var bgColorWhite []rune = []rune{ESC, LEFT_SQUARE_BRACKET, 0x20, SEMI_COLUMN, 0x31,  0x6D} // esc [
+var bgColorWhite []rune = []rune{ESC, LEFT_SQUARE_BRACKET, 0x21, M_HEX}
+//var bgColorWhite []rune = []rune{ESC, LEFT_SQUARE_BRACKET, 0x2}
+
 func main() {
 
 	fmt.Println("Start!")
-
+	var lines []string
 	pr, pw := io.Pipe()
 	c1 := make(chan int)
-	go writeToPipe(pw, c1)
-	go readFromPipe(pr, c1)
-
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	m := convertToMap(lines)
+	go writeToPipe(pw, c1, m)
+	readFromPipe(pr, c1)
 	<-c1
 
 	fmt.Println("Tschüssikovski!")
 }
 
-func writeToPipe(pw *io.PipeWriter, ch chan int) {
-	var data []rune
-	var command []rune
-	var err error
-	var r rune
-	var t *tty.TTY
+func convertToMap(str []string) map[int][]rune {
+	m := make(map[int][]rune)
+	for i, s := range str {
+		m[i] = []rune(s)
+	}
+	return m
+}
 
+func writeToPipe(pw *io.PipeWriter, ch chan int, v map[int][]rune) {
+	var err error
+	var input string
+	var t *tty.TTY
+	n := append(bgColorWhite, v[5]...)
+	fmt.Println(n)
 	if t, err = tty.Open(); err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
 
 	for {
-		if r, err = t.ReadRune(); errors.Is(io.EOF, err) {
+		if input, err = t.ReadString(); errors.Is(io.EOF, err) {
 			fmt.Println("EOF!")
-			//command = cmdSend
-			//r = runeCarriageReturn
-		} else {
-			if r == 0x0 {
-				//fmt.Println("0x0")
-				continue
-			}
-			command = append(command, r)
-			fmt.Println(command)
+			exitWriteToPipe(pw, ch)
 		}
-		if r == runeCarriageReturn {
-			fmt.Println("Carriage Return")
-			fmt.Println("Kommando: ", string(command))
-			fmt.Println("Code: ", command)
-			if compareRune(command, cmdSend) {
-				fmt.Println("Sending Message", command)
-				doWrite(pw, ch, data)
-				data = make([]rune, 0) // Daten zum senden zurücksetzen
-			} else if compareRune(command, cmdExit) {
-				break
-			} else {
-				data = append(data, command...)
-			}
-			command = make([]rune, 0) // Neuer Befehl
-		}
-		if errors.Is(io.EOF, err) {
+
+		if input == "exit" {
 			break
 		}
+		doWrite(pw, ch, n)
 	}
+	exitWriteToPipe(pw, ch)
+}
+
+func exitWriteToPipe(pw *io.PipeWriter, ch chan int) {
 	fmt.Println("Leitung wird an der Senderseite geschlossen!")
 	pw.Close()
 	ch <- 1
@@ -92,6 +99,11 @@ func compareRune(a, b []rune) bool {
 }
 
 func doWrite(pw *io.PipeWriter, ch chan int, data []rune) {
+	// var b []byte
+	////	for _, v := range data {
+	//		//b = append(b,[]byte(v)...)
+	////	}
+	fmt.Println(data)
 	if _, err := pw.Write([]byte(string(data))); errors.Is(io.ErrClosedPipe, err) {
 		fmt.Println("Schreiben in die Leitung nicht möglich. Pipe wurde geschlossen!")
 		ch <- 1
@@ -101,10 +113,14 @@ func doWrite(pw *io.PipeWriter, ch chan int, data []rune) {
 func readFromPipe(pr *io.PipeReader, ch chan int) {
 
 	defer pr.Close()
-	if _, err := io.Copy(os.Stdout, pr); errors.Is(err, io.ErrClosedPipe) {
+	//stdout := colorable.NewColorableStdout()
+	newColorable := colorable.NewColorable(os.Stdout)
+	//newColorable.Write(io.C)
+	//writer := bufio.NewWriter(os.Stdout)
+	if _, err := io.Copy(newColorable, pr); err != nil {
 		fmt.Println("Unerwartetes Ende der Leitung. Wurde geschlossen!")
-		ch <- 1
+		//ch <- 1
 	}
 	fmt.Println("Pipe wurde geschlossen!")
-	ch <- 1
+	//ch <- 1
 }

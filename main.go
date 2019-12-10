bc6eecdf7dbc06f1b4cffb6f5176ba995c154779
pacher <paul.pacher@oenb.at>
2019-12-10 16:42:19 +0100

    Save and Restore Cursor Position

--- main.go
+++ main.go
@@ -2,31 +2,35 @@ package main
 
 import (
 	"bufio"
+	"unicode/utf8"
+
 	//"bufio"
 	"errors"
 	"fmt"
 	"github.com/mattn/go-tty"
 	"io"
 	"os"
-	"github.com/mattn/go-colorable"
 )
 
-const ESC rune = 0x1b
+const ESC rune = 27
+const LEFT_SQUARE_BRACKET rune = 91
+const LF rune = 10
+const CR rune = 13
+const sRune rune = 115
+const uRune rune = 117
 
-var csi rune = 0x9b // CSI
-const LEFT_SQUARE_BRACKET rune = 0x5B
-const SEMI_COLUMN rune = 0x3B
-const M_HEX rune = 0x6D
+// cursor
+var saveCursorPosition  = []rune{ESC, LEFT_SQUARE_BRACKET, sRune}
+var restoreCursorPosition  = []rune{ESC, LEFT_SQUARE_BRACKET, uRune}
 
-var runeA rune = '\x3d'
-var runeLf rune = 0xA
-var runeCarriageReturn rune = 0xD
-var cmdSend []rune = []rune{0x73, 0x65, 0x6E, 0x64, 0xD}
-var cmdExit []rune = []rune{0x65, 0x78, 0x69, 0x74, 0xD}
+// keys
+var keyArrowDown = []rune{27,91,66}
+var keyArrowUp = []rune{27,91,65}
 
-//var bgColorWhite []rune = []rune{ESC, LEFT_SQUARE_BRACKET, 0x20, SEMI_COLUMN, 0x31,  0x6D} // esc [
-var bgColorWhite []rune = []rune{ESC, LEFT_SQUARE_BRACKET, 0x21, M_HEX}
-//var bgColorWhite []rune = []rune{ESC, LEFT_SQUARE_BRACKET, 0x2}
+// colors
+var bgColorMagenta []rune = []rune{ESC,LEFT_SQUARE_BRACKET,52,54,109}
+var bgColorReset []rune = []rune{ESC,LEFT_SQUARE_BRACKET,48,109}
+
 
 func main() {
 
@@ -37,11 +41,15 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		line := scanner.Text()
+		if utf8.ValidString(line) == false {
+			fmt.Println("Kein UTF-8!")
+		}
+		lines = append(lines, line)
 	}
 	m := convertToMap(lines)
 	go writeToPipe(pw, c1, m)
-	readFromPipe(pr, c1)
+	readFromPipe(pr)
 	<-c1
 
 	fmt.Println("Tschüssikovski!")
@@ -55,35 +63,79 @@ func convertToMap(str []string) map[int][]rune {
 	return m
 }
 
-func writeToPipe(pw *io.PipeWriter, ch chan int, v map[int][]rune) {
+func writeToPipe(pw *io.PipeWriter, ch chan int, m map[int][]rune) {
 	var err error
-	var input string
+	var key rune
+	var input []rune
 	var t *tty.TTY
-	n := append(bgColorWhite, v[5]...)
-	fmt.Println(n)
+
 	if t, err = tty.Open(); err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
-
+	runes := convertToSliceOfRunes(m)
+	runes = append(saveCursorPosition, runes...)
+	doWrite(pw, ch, runes)
+	var index int = 0
 	for {
-		if input, err = t.ReadString(); errors.Is(io.EOF, err) {
+		if key, err = t.ReadRune(); errors.Is(io.EOF, err) {
 			fmt.Println("EOF!")
 			exitWriteToPipe(pw, ch)
 		}
-
-		if input == "exit" {
-			break
+		if key == 0 {
+			continue
 		}
-		doWrite(pw, ch, n)
+		fmt.Println("Rune:", key)
+		if key == 27 || len(input) > 3 {
+			input = make([]rune,0)
+		}
+		input = append(input, key)
+
+		if compareRune(input, keyArrowUp) {
+			mh := prepareOutput(m, index)
+			runes = convertToSliceOfRunes(mh)
+			doWrite(pw, ch, runes)
+			index--
+		} else if compareRune(input, keyArrowDown) { // Key down
+			mh := prepareOutput(m, index)
+			runes = convertToSliceOfRunes(mh)
+			doWrite(pw, ch, runes)
+			index++
+		}
 	}
 	exitWriteToPipe(pw, ch)
+}
+
+func prepareOutput(originalMap map[int][]rune, index int)  map[int][]rune {
+	mh := make(map[int][]rune, len(originalMap))
+	for k, v := range originalMap {
+		if k == 0 {
+			mh[k] = append(restoreCursorPosition, v...)
+		} else if k == index {
+			highlighted := append(bgColorMagenta, v...)
+			highlighted = append(highlighted,bgColorReset...)
+			mh[k] = highlighted
+		} else {
+			mh[k] = v
+		}
+	}
+	return mh
 }
 
 func exitWriteToPipe(pw *io.PipeWriter, ch chan int) {
 	fmt.Println("Leitung wird an der Senderseite geschlossen!")
 	pw.Close()
 	ch <- 1
+}
+
+func convertToSliceOfRunes(m map[int][]rune) []rune {
+	var data []rune
+	for _, v := range m {
+		v = append(v, LF, CR)
+		data = append(data, v...)
+
+	}
+	return data
 }
 
 func compareRune(a, b []rune) bool {
@@ -99,25 +151,19 @@ func compareRune(a, b []rune) bool {
 }
 
 func doWrite(pw *io.PipeWriter, ch chan int, data []rune) {
-	// var b []byte
-	////	for _, v := range data {
-	//		//b = append(b,[]byte(v)...)
-	////	}
-	fmt.Println(data)
+
 	if _, err := pw.Write([]byte(string(data))); errors.Is(io.ErrClosedPipe, err) {
 		fmt.Println("Schreiben in die Leitung nicht möglich. Pipe wurde geschlossen!")
 		ch <- 1
 	}
 }
 
-func readFromPipe(pr *io.PipeReader, ch chan int) {
+func readFromPipe(pr *io.PipeReader) {
 
 	defer pr.Close()
-	//stdout := colorable.NewColorableStdout()
-	newColorable := colorable.NewColorable(os.Stdout)
-	//newColorable.Write(io.C)
+	//var buf []byte
 	//writer := bufio.NewWriter(os.Stdout)
-	if _, err := io.Copy(newColorable, pr); err != nil {
+	if _, err := io.Copy(os.Stdout, pr); err != nil {
 		fmt.Println("Unerwartetes Ende der Leitung. Wurde geschlossen!")
 		//ch <- 1
 	}

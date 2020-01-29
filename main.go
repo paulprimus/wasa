26c9ce38e3cd6738a9f6281586052729f3558908
pacher <paul.pacher@oenb.at>
2020-01-29 17:29:51 +0100

    starte highlight words

--- main.go
+++ main.go
@@ -23,12 +23,20 @@ var saveCursorPosition = []rune{ESC, LEFT_SQUARE_BRACKET, sRune}
 var restoreCursorPosition = []rune{ESC, LEFT_SQUARE_BRACKET, uRune}
 
 // keys
-var keyArrowDown = []rune{27, 91, 66}
-var keyArrowUp = []rune{27, 91, 65}
+var keyArrowDown = []rune{ESC, 91, 66}
+var keyArrowUp = []rune{ESC, 91, 65}
+var keyArrowRight = []rune{ESC, 91, 67}
+var keyArrowLeft = []rune{ESC, 91, 68}
 
 // colors
 var bgColorMagenta []rune = []rune{ESC, LEFT_SQUARE_BRACKET, 52, 54, 109}
 var bgColorReset []rune = []rune{ESC, LEFT_SQUARE_BRACKET, 48, 109}
+
+type Word struct {
+	index int
+	character []rune
+	highlighted bool
+}
 
 func main() {
 
@@ -40,8 +48,8 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 	var i int = 0
 	for scanner.Scan() {
-		line := scanner.Text()
-		runes := []rune(line)
+		text := scanner.Text()
+		runes := []rune(text)
 		runes = append(runes, LF)
 		lines[i] = runes
 		i++
@@ -68,6 +76,7 @@ func writeToPipe(pw *io.PipeWriter, ch chan int, m map[int][]rune) {
 	runes = append(saveCursorPosition, runes...)
 	doWrite(pw, ch, runes)
 	var index int = -1
+	var horizontalIndex = -1
 	var maxIndex int = len(m) - 1
 	for {
 		if key, err = t.ReadRune(); errors.Is(io.EOF, err) {
@@ -86,42 +95,133 @@ func writeToPipe(pw *io.PipeWriter, ch chan int, m map[int][]rune) {
 		input = append(input, key)
 
 		if compareRune(input, keyArrowUp) { // Key up
+		horizontalIndex = -1
 			if index >= 0 {
 				index--
-				mh := prepareOutput(m, index)
+				mh := prepareOutput(m, index, horizontalIndex, false)
 				runes = convertToSliceOfRunes(mh)
 				doWrite(pw, ch, runes)
 			}
 		} else if compareRune(input, keyArrowDown) { // Key down
+			horizontalIndex = -1
 			if index <= maxIndex {
 				index++
-				mh := prepareOutput(m, index)
+				mh := prepareOutput(m, index, horizontalIndex, false)
 				runes = convertToSliceOfRunes(mh)
 				doWrite(pw, ch, runes)
 			}
+		} else if compareRune(input, keyArrowRight) { // Key right
+			//if index <= maxIndex {
+			sentence := m[index]
+			words := splitSentence(sentence)
+			if horizontalIndex <= len(words) {
+				horizontalIndex++
+			}
+			mh := prepareOutput(m, index, horizontalIndex, true)
+			runes = convertToSliceOfRunes(mh)
+			doWrite(pw, ch, runes)
+			//}
+		} else if compareRune(input, keyArrowLeft) { // Key left
+			//if index <= maxIndex {
+			horizontalIndex--
+			mh := prepareOutput(m, index, horizontalIndex, true)
+			runes = convertToSliceOfRunes(mh)
+			doWrite(pw, ch, runes)
+			//}
 		}
+
 	}
 	exitWriteToPipe(pw, ch)
 }
 
-func prepareOutput(originalMap map[int][]rune, index int) map[int][]rune {
+func splitSentence(sentence []rune) map[int]Word{
+	wordmap := make(map[int]Word)
+	var word *Word
+	var character []rune
+	var wordcount int = 0
+	var lastrune rune = 32 // SPACE
+	for _, r := range sentence {
+		if ((lastrune != 32 && r == 32) || (lastrune == 32 && r != 32)) && len(character) > 0 { // neues Wort
+			word = &Word{index:wordcount, character:character}
+			wordmap[wordcount] = *word
+			wordcount++
+			character = []rune{}
+		}
+		character = append(character, r)
+		lastrune = r
+	}
+	return wordmap
+}
+
+func prepareOutput(originalMap map[int][]rune, index int, horizontalindex int, highlightword bool) map[int][]rune {
 	mh := make(map[int][]rune, len(originalMap))
 	for i := 0; i < len(originalMap); i++ {
-		//for k, v := range originalMap {
-		var consolestream []rune
-		var escapeseqence []rune
+
+		consolestream := []rune{}
+		//	var escapeseqence []rune
 		if i == 0 {
-			escapeseqence = restoreCursorPosition // Restore Cursor Position
+			consolestream = restoreCursorPosition // Restore Cursor Position
 		}
 		if index == i {
-			escapeseqence = append(escapeseqence, bgColorMagenta...)
+			if highlightword {
+				consolestream = append(consolestream, highlightWord(originalMap[i], horizontalindex)...)
+			} else {
+				consolestream = append(consolestream, bgColorMagenta...)
+				consolestream = append(consolestream, originalMap[i]...)
+				consolestream = append(consolestream, bgColorReset...)
+			}
+		} else {
+			consolestream = append(consolestream, originalMap[i]...)
 		}
-		consolestream = append(escapeseqence, originalMap[i]...) // Mark Line
-		consolestream = append(consolestream, bgColorReset...)
+		//	consolestream = append(consolestream, originalMap[i]...)
+		//if !highlightword {
+		//consolestream = append(consolestream, originalMap[i]...)
+		//consolestream = append(consolestream, bgColorReset...)
+		//}
 		mh[i] = consolestream
-
 	}
+
 	return mh
+}
+
+func highlightWord(row []rune, horizontalindex int) []rune {
+	wordmap := make(map[int][]rune)
+	var word []rune
+	var wordcount int = 0
+	var lastrune rune = 32 // SPACE
+	for _, r := range row {
+		if ((lastrune != 32 && r == 32) || (lastrune == 32 && r != 32)) && len(word) > 0 { // neues Wort
+			if wordcount == horizontalindex && !isSpaceWord(word) {
+				word = append(bgColorMagenta, word...)
+				word = append(word, bgColorReset...)
+			}
+			wordmap[wordcount] = word
+			wordcount++
+			word = []rune{}
+		}
+		word = append(word, r)
+		lastrune = r
+	}
+	var highlightedRow []rune
+	//for _, v := range wordmap {
+	for i:=0; i<len(wordmap); i++ {
+		highlightedRow = append(highlightedRow, wordmap[i]...)
+		//	highlightedRow = append(highlightedRow, 32)
+	}
+	//return append(highlightedRow, CR)
+	return highlightedRow
+}
+
+func isSpaceWord(word []rune) bool {
+	if word == nil {
+		return false
+	}
+	for _, v := range word {
+		if v == 32 {
+			return true
+		}
+	}
+	return false
 }
 
 func exitWriteToPipe(pw *io.PipeWriter, ch chan int) {
